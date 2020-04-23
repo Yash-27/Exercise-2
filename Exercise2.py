21847380a8df4a90a105a6517f0527b264e4bf88
Yash-27 <64075507+Yash-27@users.noreply.github.com>
2020-04-23 20:10:54 +0530

    Created in Python
    All the three have been included in this one.

---
+++ Exercise2.py
@@ -0,0 +1,22 @@
+# Assigning elements to different lists #
+x=input("Enter an element: ")
+y=input("Enter an element: ")
+z=input("Enter an element: ")
+print(list)
+
+# Accessing elements from a tuple #
+tuple=(1, "King", True)
+print(tuple[0])
+print(tuple[2])
+print(tuple[1])
+
+# Deleting different dictionary elements #
+dict={
+    "a":"my",
+    "b":"captain",
+    "c":"great"
+     }
+print(dict)
+print(dict["c"])
+del dict["c"]
+print(dict)
